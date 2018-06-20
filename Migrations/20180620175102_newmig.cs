6cc6e46b1b2c3f4c37c59dc19d2077f4e461445d
Khalil Jolibois <kjolibois@gmail.com>
2018-06-20 14:21:14 -0400

    reseed3

--- Migrations/20180620175102_newmig.cs
+++ Migrations/20180620175102_newmig.cs
@@ -9,52 +9,7 @@ namespace HomeBase.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-migrationBuilder.CreateTable(
-                name: "ParticipantQOTD",
-                columns: table => new
-                {
-                    ID = table.Column<int>(type: "INTEGER", nullable: false)
-                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
-                    Creator =  table.Column<string>(maxLength: 600, nullable:false),
-                    DateCreated = table.Column<DateTime>(type: "DateTime2", nullable: false),
-                    FamilyName = table.Column<string>(maxLength: 600, nullable: true),
-                    GivenName = table.Column<string>(maxLength: 600, nullable:false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_ParticipantQOTD", x => x.ID);
-                });
 
-            migrationBuilder.CreateTable(
-                name: "QOTD",
-                columns: table => new
-                {
-                    ID = table.Column<int>(type: "INTEGER", nullable: false)
-                         .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
-                    Question = table.Column<string>(maxLength: 600, nullable:false),
-                    QuestionAuthor = table.Column<string>(maxLength: 600, nullable: true),
-                    QuestionDate = table.Column<DateTime>(type: "DateTime2", nullable: false),
-                    Theme = table.Column<string>(maxLength: 600, nullable:false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_QOTD", x => x.ID);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "QOTDAnswer",
-                columns: table => new
-                {
-                    ID = table.Column<int>(type: "INTEGER", nullable: false)
-                         .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
-                    Answer = table.Column<string>(maxLength: 600, nullable: true),
-                    Question = table.Column<string>(maxLength: 600, nullable:false),
-                    QuestionDate = table.Column<DateTime>(type: "DateTime2", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_QOTDAnswer", x => x.ID);
-                });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
---
+++ Migrations/20180620182039_newmigs.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace HomeBase.Migrations
+{
+    public partial class newmigs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+migrationBuilder.CreateTable(
+                name: "ParticipantQOTD",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Creator =  table.Column<string>(maxLength: 600, nullable:false),
+                    DateCreated = table.Column<DateTime>(type: "DateTime2", nullable: false),
+                    FamilyName = table.Column<string>(maxLength: 600, nullable: true),
+                    GivenName = table.Column<string>(maxLength: 600, nullable:false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ParticipantQOTD", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "QOTD",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                         .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Question = table.Column<string>(maxLength: 600, nullable:false),
+                    QuestionAuthor = table.Column<string>(maxLength: 600, nullable: true),
+                    QuestionDate = table.Column<DateTime>(type: "DateTime2", nullable: false),
+                    Theme = table.Column<string>(maxLength: 600, nullable:false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QOTD", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "QOTDAnswer",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                         .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Answer = table.Column<string>(maxLength: 600, nullable: true),
+                    Question = table.Column<string>(maxLength: 600, nullable:false),
+                    QuestionDate = table.Column<DateTime>(type: "DateTime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QOTDAnswer", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
