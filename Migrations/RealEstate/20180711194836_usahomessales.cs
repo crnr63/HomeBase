2e46a48b618604271d9321d0841aef51383a454e
Khalil Jolibois <kjolibois@gmail.com>
2018-07-12 00:33:24 -0400

    clearbutton on calculator nullable fields on real estate

--- Migrations/RealEstate/20180711194836_usahomessales.cs
+++ Migrations/RealEstate/20180711194836_usahomessales.cs
@@ -8,25 +8,7 @@ namespace HomeBase.Migrations.RealEstate
     public partial class usahomessales : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
-        { migrationBuilder.CreateTable(
-                name: "USAHomeSales",
-                columns: table => new
-                {
-                   ID = table.Column<int>(type: "INTEGER", nullable: false)
-                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
-     
-
-                    finalsalesprice=table.Column<decimal>(),
-                    listingprice=table.Column<string>(maxLength: 600, nullable:false),
-                    notes=table.Column<string>(maxLength: 600, nullable:false),
-                    parsedhomeID = table.Column<string>(maxLength: 600, nullable:false),
-                    snapshotdate = table.Column<DateTime>(type: "DateTime2", nullable: false),
-                    snapshottype = table.Column<string>(maxLength: 600, nullable:false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_USAHouses", x => x.ID);
-                });
+        { 
 
            
         }
---
+++ Migrations/RealEstate/20180712024829_usahomessalesnullable.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace HomeBase.Migrations.RealEstate
+{
+    public partial class usahomessalesnullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+migrationBuilder.CreateTable(
+                name: "USAHomeSales",
+                columns: table => new
+                {
+                   ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+     
+
+                    finalsalesprice=table.Column<decimal>(nullable:true),
+                    listingprice=table.Column<string>(maxLength: 600, nullable:true),
+                    notes=table.Column<string>(maxLength: 600, nullable:false),
+                    parsedhomeID = table.Column<string>(maxLength: 600, nullable:false),
+                    snapshotdate = table.Column<DateTime>(type: "DateTime2", nullable: false),
+                    snapshottype = table.Column<string>(maxLength: 600, nullable:false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_USAHouses", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
